eb82c987992b1d9b13e378a3e144c2afd8d4bbe0
oliver <1401594271@qq.com>
2022-11-23 09:32:47 +0800

    init project

---
+++ src/test/java/com/oliver/devops_test/DevopsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliver.devops_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
